04716c30f2db45e020c74f193a6260390bed7534
Hajoo Chung <hajoo.chung@gmail.com>
2020-10-15 15:53:50 +0100

    building functions...

--- models/score.js
+++ models/score.js
@@ -5,10 +5,14 @@ const {query} = require("../db")
 async function getAllScores() {
     const result = await query("SELECT * FROM scoreChecker;");
     return result.rows;
-  }
+}
 
-  module.exports = {
-    getAllScores
-    
-  };
-  
+async function addScore(name) {
+  const result = await query("INSERT INTO name(text) VALUES ($1) RETURNING id;", [name]);
+  return result.rows.id;
+}
+
+module.exports = {
+  getAllScores,
+  addScore
+}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
- var app  = express()
+var app  = express()
+const { getAllScores, addScore } = require ("../models/score");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,12 +12,17 @@ router.get('/', function(req, res, next) {
 module.exports = router;
 
 // Get requests
-router.get("/", async function (req, res){ 
-
-const score = await getAllScores();
+router.get("/", async function (req, res){
+  const score = await getAllScores();
   res.json({success: true, payload:score});
-
 })
 
 
 // Post requests
+
+router.post("/", async function (req, res) {
+  // const name = req.body;
+  // const id = await addScore(name);
+  //res.json({ success: true, message: `Your score has been uploaded...${id}` });
+  //res.send ('Hello');
+});
