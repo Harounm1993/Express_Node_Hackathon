ce7a64d955c0918e5038e562c19ff962d5e31792
Hajoo Chung <hajoo.chung@gmail.com>
2020-10-16 11:06:11 +0100

    function

--- public/main.js
+++ public/main.js
@@ -1,5 +1,11 @@
 let myScore = "";
-let myName = 
+let myName = document.querySelector('#user-name option[value=valueB]').text;
+//console.log(myName);
+let btn1 = document.querySelector("#submit");
+let btn2 = document.querySelector("#get-score");
+//console.log(btn1);
+btn2.addEventListener ("click", getApi);
+
 
 async function getApi(){
     let response = await fetch (`/scores`, {
@@ -8,9 +14,29 @@ async function getApi(){
     myScore = data.payload[0].name;
     console.log(myScore);
 
-    if (myName === myScore){
+    // if (myName === myScore){
 
-    }
+    // }
 };
 getApi();
 
+
+function showScore(){
+
+}
+
+
+
+function readScore (string) {
+    let p = document.createElement("p")
+    p.innerText = string;
+    score.appendChild(p);
+    if 
+};
+
+
+
+
+
+10:58
+let score = document.querySelector(".scoreReader")
